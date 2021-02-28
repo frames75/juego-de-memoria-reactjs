a188419a8210e709ab5bbf02998f7f561d9c7ecf
frames75 <5886604+frames75@users.noreply.github.com>
2021-02-28 10:51:58 +0100

    popup funct

--- src/PopupFinal.js
+++ src/PopupFinal.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Popup from './utils/Popup';
 
-export default class PopupFinal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: '¡Terminaste en ' + props.intentos + ' intentos!'
-    }
-  }
-
-  render() {
-    return (
-      <Popup
-          text={this.state.text}
-          closePopup={this.props.closePopup}
-      />
-    );
-  }
+export default function PopupFinal(props) {
+  const texto = '¡Terminaste en ' + props.intentos + ' intentos!';
+  
+  return (
+    <Popup>
+      <h2>{texto}</h2>
+      <br />
+      <button className='cerrar' 
+        onClick={props.closePopup}>Cerrar</button>
+    </Popup>
+  );
 };
--- src/utils/Popup.js
+++ src/utils/Popup.js
@@ -1,19 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Popup.css';
 
-export default class Popup extends Component {
-  render() {
-    return (
-      <div className='popup'>
-        <div className='popup_inner'>
-          <div className='popup_text'>
-            <h2>{this.props.text}</h2>
-            <br />
-            <button className='cerrar' 
-              onClick={this.props.closePopup}>Cerrar</button>
-          </div>
+export default function Popup(props) {
+  return (
+    <div className='popup'>
+      <div className='popup_inner'>
+        <div className='popup_text'>
+          {props.children}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 };
